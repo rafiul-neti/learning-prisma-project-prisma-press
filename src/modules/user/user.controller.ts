1cc5ac123710456a1dd6526e442d09af45fb9d84
Rafiul Islam <irafiul210@gmail.com>
2026-07-21 16:06:36 +0600

    feat: add sendSuccessResponse utility in src/utils/sendSuccessResponse.ts

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,14 +2,14 @@ import { Request, Response } from "express";
 import httpStatus from "http-status";
 import { userService } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendSuccessResponse } from "../../utils/sendSuccessResponse";
 
 const registerUser = catchAsync(async (req: Request, res: Response) => {
   const user = await userService.registerUserIntoDB(req.body);
 
-  res.status(httpStatus.CREATED).json({
-    success: true,
+  sendSuccessResponse(res, {
     statusCode: httpStatus.CREATED,
-    message: "Users registered successfuly!",
+    message: "User registered successfully!",
     data: { user },
   });
 });
